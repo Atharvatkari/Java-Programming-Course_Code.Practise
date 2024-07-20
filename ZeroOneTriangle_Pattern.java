1e268004bd086a31c4e3ad29fec861093ccec640
Atharva Atkari <148195541+Atharvatkari@users.noreply.github.com>
2024-07-20 12:09:44 +0530

    Add files via upload

---
+++ ZeroOneTriangle_Pattern.java
@@ -0,0 +1,18 @@
+public class ZeroOneTriangle_Pattern {
+    public static void pattern(int num){
+        for(int i=1; i<=num; i++){
+            for(int j=1; j<=i; j++){
+                if((i+j)%2 == 0){// even
+                    System.out.print("1");
+                }
+                else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pattern(10);
+    }
+}
